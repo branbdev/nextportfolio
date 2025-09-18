45ae6bc77408e6ded52139f0a0f88f97c796dd44
Brandon Bowen <aghoraphobic@gmail.com>
2025-09-18 04:20:10 -0700

    chore: cleaning up CSS and creating pre-update checkpoint

--- pages/index-light.js
+++ pages/index-light.js
@@ -2,9 +2,7 @@ import { useEffect } from 'react';
 import About from '../src/components/About';
 import Contact from '../src/components/Contact';
 import Home from '../src/components/Home';
-import News from '../src/components/News';
 import Portfolio from '../src/components/Portfolio';
-import Services from '../src/components/Services';
 import Layout from '../src/layouts/Layout';
 
 const Index = () => {
@@ -21,12 +19,6 @@ const Index = () => {
       {/* Portfolio Section */}
       <Portfolio />
       {/* /Portfolio Section */}
-      {/* Services Section */}
-      <Services />
-      {/* /Services Section */}
-      {/* News Section */}
-      <News />
-      {/* /News Section */}
       {/* Contact Section */}
       <Contact />
       {/* /Contact Section */}
