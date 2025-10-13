87bd3dea0324d9364aacd29edb26c04b80225fcb
Brandon Bowen <brandonbowenbiz@gmail.com>
2025-10-12 18:24:23 -0700

    Installed dependencies for a blog feature including Apollo server, GraphQL, and graymatter. Structured data entities into blog, projects, and technologies. Restructured portfolioData tags to be interchangeable between posts and current projects. Added taxonomies to filter items by tags and suggest related content containing identical tags. Added 'Latest Articles' component to main page and populated it with dummy post data.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { Fragment } from 'react';
 import '../styles/globals.css';
 import { ContextProvider } from '../src/context/Context';
 import Layout from '../src/layouts/Layout';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../src/lib/graphql/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
+  // Wrap Apollo client initialization in a try-catch to handle potential errors
+  let apolloClient;
+  try {
+    apolloClient = useApollo(pageProps.initialApolloState || {});
+  } catch (error) {
+    console.error("Error initializing Apollo client:", error);
+    // We'll render without Apollo if there's an error
+  }
+
+  // If apolloClient is undefined, render without Apollo Provider
+  return apolloClient ? (
+    <ApolloProvider client={apolloClient}>
+      <ContextProvider>
+        <Fragment>
+          <Head>
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link
+              rel='preconnect'
+              href='https://fonts.gstatic.com'
+              crossOrigin=''
+            />
+            <link
+              href='https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+              rel='stylesheet'
+            />
+            <link rel='icon' href='/svg/favicon.svg' />
+            <meta
+              name='description'
+              content='Portfolio for Brandon Bowen, a Full-Stack software engineer based in Corona, CA, specializing in building scalable enterprise applications with .NET, React, and Python. Available for contract, freelance projects and full-time opportunities.'
+            />
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1, shrink-to-fit=no'
+            />
+            <meta name='robots' content='index, follow' />
+            <meta
+              name='google-site-verification'
+              content='C4CjLzj7j9qMsKKZ4E0W4AxpsHWED17IYeQ8hgelrN0'
+            />
+          </Head>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Fragment>
+      </ContextProvider>
+    </ApolloProvider>
+  ) : (
     <ContextProvider>
       <Fragment>
         <Head>
